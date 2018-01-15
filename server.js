eb6c7e7f259d8c201222581b0c643854131f39e9
jjade seravla <jadealvares@hotmail.co.uk>
2018-01-15 14:25:17 +0000

    cookie-parser and express middleware installed

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session'); // brings in modules so we can use them
+
 var morgan = require('morgan'); //logs
 
 app.use(morgan('dev')); //request to server will use app morgan
-
+app.use(cookieParser()); //sets request.cookies variable
+app.use(session({secret: 'anystringoftext',// requires 3 things, secret is a key code that its gona require for cookies
+                          saveUninitialized: true,// if a session comes in and its not initialized, will still save on database
+                        })) // even if nothing is saved you can still save to database
 app.use('/', function(req, res){
   res.send('hello');
 });
